d4d9ada79acf2e29704c3812ef6a8158d1ac843e
Paolo Fabris <paolo.fabris.1992@gmail.com>
2022-04-21 14:47:52 +0200

    clena code sintax and change forigate configuration

--- fortinet.sh
+++ fortinet.sh
@@ -48,9 +48,8 @@ ip2dec() {
 }
 
 SetStatic() {
-  local regex=$(printf '10000')
-  cat $1 | grep -oE ${regex} | while read id; do
-    ((newstatic = id + 64))
+  cat $1 | grep -oE 10000 | while read id; do
+    ((newstatic = id + ID))
     sed -i -E "s|(\b)($id)(\b)|\1${newstatic}\3|i" $1
     break
   done
@@ -58,41 +57,36 @@ SetStatic() {
   return 0
 }
 
-Fortigate40F() {
-  local regex=$(printf '((010|10)\%s(([0-9]{1,3}|1[0-9][0-9]|2([0-4][0-9]|5[0-5]))\%s){2}([0-9]{1,3}|1[0-9][0-9]|2([0-4][0-9]|5[0-5])))' $1 $1)
+Fortigate() {
+  local regex=$(printf '(([0-9]{1,3}|1[0-9][0-9]|2([0-4][0-9]|5[0-5]))\%s){2,3}([0-9]{1,3}|1[0-9][0-9]|2([0-4][0-9]|5[0-5]))' $1)
+  local named='^(([0-1][0-9][0-9]|2([0-4][0-9]|5[0-5]))\-){2}([0-9]{1,3}|1[0-9][0-9]|2([0-4][0-9]|5[0-5]))$'
   local array=()
 
   cat $2 | grep -oE ${regex} | while read ip; do
-    toint=$(ip2dec $ip $1)
+
+    if [[ $ip =~ ${named} ]]; then
+      toint="92-$ip"
+      toint=$(ip2dec $toint $1)
+    else
+      ! [[ $ip =~ ^10 ]] && continue;
+      toint=$(ip2dec $ip $1)
+    fi
+
     [[ $toint =~ 181700296|181700306|167772160|176422915 ]] && continue
     [[ ${array[*]} == *"$toint"* ]] && continue
     array+=($toint)
 
     ((toint = toint + ID))
     replace=$(dec2ip $toint $1)
+
+    [[ $ip =~ ${named} ]] && replace=${replace:4}
     sed -i -E "s|(\b)($ip)(\b)|\1${replace}\3|i" $2
+
   done
 
   return 0
 }
 
-FortigateVM() {
-  local regex=$(printf '(([0-9]{1,3}|1[0-9][0-9]|2([0-4][0-9]|5[0-5]))\-){2}([0-9]{1,3}|1[0-9][0-9]|2([0-4][0-9]|5[0-5]))\-%s' $1)
-  cat $2 | grep -oE ${regex} | while read identity; do
-    length=$(echo $1 | wc -c)
-    ip=$(sed "s/.\{$length\}$//" <<<"$identity")
-    ip="10-$ip"
-
-    toint=$(ip2dec $ip '-')
-    ((toint = toint + ID))
-    replace=$(dec2ip $toint '-')
-    replace=${replace:3}
-
-    sed -i -E "s|(\b)($identity)(\b)|\1${replace}\-${1}\3|i" $2
-  done
-
-  return 0
-}
 
 TEMPORARY=$(mktemp -d)
 F40="${TEMPORARY}/40F.conf"
@@ -103,13 +97,8 @@ cp ./template/FVM.conf $FVM
 
 SYMBOLS=("." "-")
 for s in "${SYMBOLS[@]}"; do
-  Fortigate40F $s $F40
-  Fortigate40F $s $FVM
-done
-
-INTERTFACES=("A" "W")
-for i in "${INTERTFACES[@]}"; do
-  FortigateVM $i $FVM
+  Fortigate $s $F40
+  Fortigate $s $FVM
 done
 
 SetStatic $FVM
